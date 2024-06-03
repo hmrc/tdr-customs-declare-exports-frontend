103839031595f3f649ad07757f926e5d8cb9ee8d
Tim <glidester@gmail.com>
2024-06-03 12:59:08 +0100

    CEDS-5845 Renaming service

--- docs/Customs Declare Exports Auth AutoComplete.js
+++ docs/Customs Declare Exports Auth AutoComplete.js
@@ -6,7 +6,7 @@
 // @author    You
 // @match     http*://*/auth-login-stub/gg-sign-in?continue=*customs-declare-exports*
 // @grant     none
-// @updateURL https://raw.githubusercontent.com/hmrc/customs-declare-exports-frontend/master/docs/Customs%20Declare%20Exports%20Auth%20AutoComplete.js
+// @updateURL https://raw.githubusercontent.com/hmrc/tdr-customs-declare-exports-frontend/master/docs/Customs%20Declare%20Exports%20Auth%20AutoComplete.js
 // ==/UserScript==
 
 (function() {
